7d83a55cdd6c32202448da8275c80fbee5144489
Juanro Alvarado <juanroberto.alvarado14@outlook.com>
2018-05-20 17:14:11 -0600

    Fixed and added the delta

--- trapezoidIntegral.hs
+++ trapezoidIntegral.hs
@@ -1,9 +1,8 @@
 func x = sin(x)
 
-integrate :: Float -> Float -> Float -> Float -> Float
-integrate x n lim0 lim1
-    | lim0 >= lim1 = (x * (lim1/n))
-    |  x == 0 = integrate (x + (lim1/n)/2 * (func lim0 + func lim1)) n (lim0+(lim1/n)) lim1
-    | otherwise = integrate (x + func lim0) n (lim0+(lim1/n)) lim1
-
-
+integrate :: Float -> Float -> Float -> Float -> Float -> Float
+integrate x n lim0 newlim0 lim1 
+    | newlim0 >= lim1 = (x * delta)
+    |  x == 0 = integrate (x +  (func lim0 + func lim1)/2) n lim0 (newlim0+delta) lim1
+    | otherwise = integrate (x + func newlim0) n lim0 (newlim0+delta) lim1
+    where delta = ((lim1-lim0)/n)
